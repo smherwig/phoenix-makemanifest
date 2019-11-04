60a2cbbe361aa0fee36aa6a248e3b694001e1f02
smherwig <smherwig@cs.umd.edu>
2019-11-04 14:10:09 -0500

    rm gen_keypair.sh

--- gen_keypair.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# generate 2048 bit RSA keypair; output private key
-openssl genrsa -3 -out private.pem 3072
-
-# export public key
-#openssl rsa -in private.pem -outform PEM -pubout -out public.pem
